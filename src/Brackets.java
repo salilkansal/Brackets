3883414050c75ced5de33bd18bb2b609400a4455
salilkansal <salilkansal.92@gmail.com>
2015-12-02 20:24:38 -0600

    Converted the program to a function

--- src/Brackets.java
+++ src/Brackets.java
@@ -4,30 +4,32 @@ import java.util.Stack;
 
 public class Brackets {
     public static void main(String[] args) {
-        Stack<Character> myStack = new Stack<>();
         Scanner s1 = new Scanner(System.in);
-        boolean finished = false;
         String input = s1.nextLine();
 
+        System.out.println(braces(input));
+
+    }
+    static String braces(String input){
+        if(input.length()%2!=0)
+            return "NO";
+        Stack<Character> myStack = new Stack<>();
         HashMap<Character,Character> myHashmap = new HashMap<>();
         myHashmap.put('{','}');
         myHashmap.put('(',')');
         myHashmap.put('[',']');
-
         int i=0;
 
-        while (i<input.length() && !finished){
+        while (i<input.length()){
             if (myHashmap.containsKey(input.charAt(i))){
                 myStack.push(input.charAt(i));
             }
             else if (myStack.empty() || (input.charAt(i) != myHashmap.get(myStack.pop()))) {
-                finished = true;
+                return "NO";
             }
             i++;
         }
-            if (!finished){
-                System.out.printf("YES");
-        }
-        else System.out.printf("NO");
+        return "YES";
     }
 }
+
